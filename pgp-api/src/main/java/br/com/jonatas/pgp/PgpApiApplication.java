1ba9bb8f16db8b1a4237d1c2c6595b40736aa784
jonatasfonsec <jonatas.fonsec@gmail.com>
2026-03-08 09:02:40 -0300

    Commit Inicial - Projeto Spring Boot PGP

---
+++ pgp-api/src/main/java/br/com/jonatas/pgp/PgpApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonatas.pgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgpApiApplication.class, args);
+	}
+
+}
